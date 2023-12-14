d3067a6a71aa33ce7ab978019d929e9c72ed365a
helauren42 <helauren42@gmail.com>
2023-12-14 15:00:28 +0100

    ehwurie

---
+++ src/parsing/get_file_fd.c
@@ -0,0 +1,15 @@
+#include "../../include/minirt.h"
+
+int	get_file_fd(char *s)
+{
+	int	i;
+	int	fd;
+
+	if(ft_strncmp(s, "scenes/", 7))
+		return (-2);
+	i = ft_strlen(s) -1;
+	if(s[i] != 't' && s[i] != 'r' && s[i] != '.')
+		return (-2);
+	fd = open(s, O_RDONLY, 0644);
+	return (fd);
+}
