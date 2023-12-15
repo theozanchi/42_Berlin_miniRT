4268784bd5e94a7d1b2b5379ca708809ccbace28
tzanchi <tzanchi@student.42berlin.de>
2023-12-15 16:39:43 +0100

    theo functionalities init

--- src/free/free.c
+++ src/free/free.c
@@ -6,9 +6,34 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 17:11:31 by helauren          #+#    #+#             */
-/*   Updated: 2023/12/14 15:36:17 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/12/15 16:30:55 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minirt.h"
 
+void	free_objects(t_data *data)
+{
+	t_object	*current;
+	t_object	*temp;
+
+	current = data->first;
+	while (current)
+	{
+		temp = current;
+		current = current->next;
+		if (temp)
+			free_and_set_to_null(2, temp->data, temp);
+	}
+	data->first = NULL;
+}
+
+int	free_resources_and_quit(t_data *data)
+{
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_image(data->mlx_ptr, data->img.img);
+	free_objects(data);
+	free_and_set_to_null(3, data->ambient_lighting, data->camera, data->light);
+	exit(EXIT_SUCCESS);
+	return (0);
+}
