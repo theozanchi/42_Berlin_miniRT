2187480127a920c3425ffb2c1f18fd5ba98183d5
tzanchi <tzanchi@student.42berlin.de>
2023-12-15 09:57:36 +0100

    commit before merge

--- src/ray_tracing/render_scene.c
+++ src/ray_tracing/render_scene.c
@@ -6,9 +6,32 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 16:05:37 by tzanchi           #+#    #+#             */
-/*   Updated: 2023/12/14 16:06:17 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/12/14 20:29:16 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minirt.h"
+#define BACKGROUND_COLOUR 255
 
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+// To better understand: the equation of a ray is P = O + t * (V - O)
+
+void	render_scene(t_data *data)
+{
+	size_t	i;
+
+	data->img.addr = mlx_get_data_addr(data->img.img, &data->img.bits_per_pixel,
+			&data->img.line_length, &data->img.endian);
+	i = 0;
+	while (i < data->img.line_length)
+	{
+		
+	}
+}
