fb4b70c5e99656714b4807b69966515abcea6db6
tzanchi <tzanchi@student.42berlin.de>
2023-12-17 20:00:40 +0100

    added counter for loading

--- src/algebra_utils/matrix_calculation.c
+++ src/algebra_utils/matrix_calculation.c
@@ -10,61 +10,61 @@
 // /*                                                                            */
 // /* ************************************************************************** */
 
-// #include "../../inc/algebra.h"
+#include "../../inc/algebra.h"
 
-// t_vec3	vec_add(t_vec3 A, t_vec3 B)
-// {
-// 	t_vec3	output;
+t_vec3	vec_add(t_vec3 A, t_vec3 B)
+{
+	t_vec3	output;
 
-// 	output.x = A.x + B.x;
-// 	output.y = A.y + B.y;
-// 	output.z = A.z + B.z;
-// 	return (output);
-// }
+	output.x = A.x + B.x;
+	output.y = A.y + B.y;
+	output.z = A.z + B.z;
+	return (output);
+}
 
-// t_vec3	vec_sub(t_vec3 A, t_vec3 B)
-// {
-// 	t_vec3	output;
+t_vec3	vec_sub(t_vec3 A, t_vec3 B)
+{
+	t_vec3	output;
 
-// 	output.x = A.x - B.x;
-// 	output.y = A.y - B.y;
-// 	output.z = A.z - B.z;
-// 	return (output);
-// }
+	output.x = A.x - B.x;
+	output.y = A.y - B.y;
+	output.z = A.z - B.z;
+	return (output);
+}
 
-// t_vec3	vec_mul(t_vec3 A, t_vec3 B)
-// {
-// 	t_vec3	output;
+t_vec3	vec_mul(t_vec3 A, t_vec3 B)
+{
+	t_vec3	output;
 
-// 	output.x = A.x * B.x;
-// 	output.y = A.y * B.y;
-// 	output.z = A.z * B.z;
-// 	return (output);
-// }
+	output.x = A.x * B.x;
+	output.y = A.y * B.y;
+	output.z = A.z * B.z;
+	return (output);
+}
 
-// float	dot_product(t_vec3 A, t_vec3 B)
-// {
-// 	return (A.x * B.x + A.y * B.y + A.z * B.z);
-// }
+double	dot_product(t_vec3 A, t_vec3 B)
+{
+	return (A.x * B.x + A.y * B.y + A.z * B.z);
+}
 
-// t_vec3	vec_mul_scalar(t_vec3 v, float t)
-// {
-// 	v.x *= t;
-// 	v.y *= t;
-// 	v.z *= t;
-// 	return (v);
-// }
+t_vec3	vec_mul_scalar(t_vec3 v, double t)
+{
+	v.x *= t;
+	v.y *= t;
+	v.z *= t;
+	return (v);
+}
 
-// t_vec3	vec_normalize(t_vec3 v)
-// {
-// 	float length;
+t_vec3	vec_normalize(t_vec3 v)
+{
+	float length;
 	
-// 	length = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-//     if (length)
-//     {
-//         v.x /= length;
-//         v.y /= length;
-//         v.z /= length;
-//     }
-// 	return (v);
-// }
+	length = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+    if (length)
+    {
+        v.x /= length;
+        v.y /= length;
+        v.z /= length;
+    }
+	return (v);
+}
