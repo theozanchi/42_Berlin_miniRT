bdecb6e533baa35a81623b2dbfaf6fe08a4018de
helauren42 <helauren42@gmail.com>
2023-12-18 19:24:43 +0100

    pre pull

---
+++ src/ray_tracing/check_intersection.c
@@ -0,0 +1,29 @@
+// #include "../../inc/minirt.h"
+
+// int	check_spheres(t_vec3 curr, t_object *obj)
+// {
+// 	while(obj != NULL)
+// 	{
+// 		if(obj->id == SPHERE)
+// 		{
+
+// 		}
+// 		obj = obj->next;
+// 	}
+// }
+
+// int	check_intersection(t_vec3 curr, t_data *data)
+// {
+// 	if(check_spheres(curr, data->first))
+// 	{
+
+// 	}
+// 	if(check_plane(curr, data->first))
+// 	{
+		
+// 	}
+// 	if(check_cylinder(curr, data->first))
+// 	{
+		
+// 	}
+// }
--- src/ray_tracing/launch_ray.c
+++ src/ray_tracing/launch_ray.c
@@ -1,43 +1,50 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   launch_ray.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: helauren <helauren@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/16 20:01:36 by helauren          #+#    #+#             */
-/*   Updated: 2023/12/17 01:44:03 by helauren         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   launch_ray.c                                       :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: helauren <helauren@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2023/12/16 20:01:36 by helauren          #+#    #+#             */
+// /*   Updated: 2023/12/17 15:39:15 by helauren         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include "../../inc/minirt.h"
+// #include "../../inc/minirt.h"
 
-void	launch_ray(t_ray *rays, t_data *data)
-{
-	int	i;
+// t_vec3	find_opposite_side()
+// {
+	
+// }
 
-	i = 0;
-	// reverse_ray() draw_element();
-}
+// void	launch_ray(t_ray *rays, t_data *data)
+// {
+// 	int		i;
+// 	t_vec3	opp;
 
-void	ray_after_ray(t_ray *rays, t_data *data)
-{
-	int	x;
-	int	y;
-	double	angle_yn;
+// 	i = 0;
+// 	opp = find_opposite_side();
+	
+// }
 
-	x = 0;
-	while(x < data->window->width)
-	{
-		y = 0;
-		rays->angle = rays->angle_n * (double)x;
-		angle_yn = rays->angle_n;
-		while(y < data->window->height)
-		{
-			angle_yn = rays->angle_n * (double)y;
-			launch_ray();
-			y++;
-		}
-		x++;
-	}
-}
+// void	ray_after_ray(t_ray *rays, t_data *data)
+// {
+// 	unsigned int	x;
+// 	unsigned int	y;
+// 	double	angle_yn;
+
+// 	x = 0;
+// 	while(x < data->window->width)
+// 	{
+// 		y = 0;
+// 		rays->angle = rays->angle_n * (double)x;
+// 		angle_yn = rays->angle_n;
+// 		while(y < data->window->height)
+// 		{
+// 			angle_yn = rays->angle_n * (double)y;
+// 			launch_ray(rays, data);
+// 			y++;
+// 		}
+// 		x++;
+// 	}
+// }
