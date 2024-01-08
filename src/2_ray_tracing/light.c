fbc36be965623edc76bca3e573e4fe747784921e
tzanchi <tzanchi@student.42berlin.de>
2024-01-08 16:44:14 +0100

    uncommented some light functions

--- src/2_ray_tracing/light.c
+++ src/2_ray_tracing/light.c
@@ -6,7 +6,7 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 15:24:49 by tzanchi           #+#    #+#             */
-/*   Updated: 2024/01/08 15:49:02 by tzanchi          ###   ########.fr       */
+/*   Updated: 2024/01/08 16:43:22 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,5 @@ double	get_angle_with_light(t_vec3 n, t_point3 hitted_point, t_data *data)
 	ray = vec_normalize(vec_sub(data->light->pos, hitted_point));
 	return (acos(dot_product(n, ray)));
 }
+
+
